3d54d780c3683805542b568de089710ea0d4bed1
加缪 <jiamiu.hzy@alibaba-inc.com>
2014-10-22 23:21:55 +0800

    add router download, and auto release 0.0.6

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var _ = require('lodash')
+var _ = require('lodash'),
+  path = require('path')
 
 
 module.exports = {
@@ -10,11 +11,22 @@ module.exports = {
       _.forEach( module.models ,function(model){
         if( model.isFile ){
           root.files[model.identity] = model
+
           root.dep.request.add('POST /'+model.identity, function handlerFileUpload(req,res, next){
             req.body = _.isObject( req.body) ? req.body : {}
             _.extend( req.body, _.values(req.files).pop())
             next()
           },{first:true})
+
+          root.dep.request.add('GET /'+model.identity + "/:id/download", function handlerFileDownload( req, res, next){
+            root.dep.model.models[model.identity].findOne({id:req.param('id')}).then(function( file){
+              if( file ){
+                return res.sendFile( path.join(process.cwd(), file.path ))
+              }else{
+                res.status(404).end()
+              }
+            })
+          })
         }
       })
     }
